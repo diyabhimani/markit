92eb38460a6096a684aa7598d1bc2440353186e8
diyabhimani <diyabhimani@gmail.com>
2026-05-04 17:24:41 +0530

    Add files via upload

---
+++ indexmarkit.php
@@ -0,0 +1,27 @@
+echo "<li><a href='diya/diya_attendance_recordsdelete.php'>attendance record Delete</a></li>";
+echo "<li><a href='diya/diya_attendance_recordsinsert.php'>attendance record insert</a></li>";
+echo "<li><a href='diya/diya_attendance_recordsupdate.php'>attendance record upddate </a></li>";
+echo "<li><a href='diya/diya_attendance_recordsview.php'>attendance record view </a></li>";
+
+echo "<li><a href='diya/diya_attendancedelete.php'>attendance Delete</a></li>";
+echo "<li><a href='diya/diya_attendanceinsert.php'>attendance insert</a></li>";
+echo "<li><a href='diya/diya_attendanceupdate.php'>attendance update</a></li>";
+echo "<li><a href='diya/diya_attendanceview.php'>attendance view</a></li>";
+
+echo "<li><a href='diya/diya_classesdelete.php'>class delete</a></li>";
+echo "<li><a href='diya/diya_classesinsert.php'>class insert</a></li>";
+echo "<li><a href='diya/diya_classesupdate.php'>class  update</a></li>";
+echo "<li><a href='diya/diya_classesview.php'>class view</a></li>";
+
+echo "<li><a href='diya_studentsdelete.php'>student delete</a></li>";
+echo "<li><a href='diya/diya_studentssinsert.php'>student insert</a></li>";
+echo "<li><a href='diya/diya_studentssupdate.php'>student  update</a></li>";
+echo "<li><a href='diya/diya_studentsview.php'>student view</a></li>";
+
+
+echo "<li><a href='diya/diya_usersinsert.php'>user insert</a></li>";
+echo "<li><a href='diya/diya_usersview.php'>user view</a></li>";
+echo "<li><a href='diya/diya_usersupdate.php'>user update</a></li>";
+echo "<li><a href='diya/diya_usersdelete.php'>user delete</a></li>";
+
+
